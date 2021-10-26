be1c72ca5bd92be3538305b1146ce81b9d0b5018
Gustav Frid <gustav.frid@gmail.com>
2021-10-26 20:24:08 +0200

    add title to editor

--- code/src/components/QuestionEditor/EditText.js
+++ code/src/components/QuestionEditor/EditText.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const EditText = ({ question }) => {
-  console.log(question.options)
+  const [changedQuestion, setChangedQuestion] = useState({ ...question })
+  console.log(changedQuestion)
+  const handleChangeQuestion = params => {}
+
   return (
-    <div>
+    <div className='edit-question-container'>
+      <label className='label-text-input label'>
+        Question Title
+        <span className='required'> *required</span>
+        <input
+          className='text-input'
+          required={true}
+          type='text'
+          id={question.id}
+          placeholder={question.title}
+          value={question.title}
+          onChange={() => console.log('hej')}
+        />
+      </label>
       {question.options.map(option => {
         return (
           <label className='label-text-input label' key={option.id}>
-            {option.label}
-            <span className='required'>{question.required ? ' *required' : ''}</span>
+            Label
+            <span className='required'> *required</span>
             <input
               className='text-input'
-              required={question.required === 'true'}
+              required={true}
               type='text'
               id={option.id}
-              placeholder={option.placeholder}
-              value={option.value}
+              placeholder={option.label}
+              value={option.label}
               onChange={() => console.log('hej')}
             />
           </label>
